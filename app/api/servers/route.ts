ab78c327a22e0a9334f51a6bc36b9057a3850af2
Panda-Dev <anhtuan.panda.dev@gmail.com>
2024-09-20 18:07:56 +0700

    feat: server-creation-api

---
+++ app/api/servers/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+import { MemberRole } from "@prisma/client";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function POST(request: Request) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const { imageUrl, name } = await request.json();
+
+    const server = await db.server.create({
+      data: {
+        imageUrl,
+        name,
+        profileId: profile.id,
+        inviteCode: uuidv4(),
+        channels: {
+          create: [
+            {
+              name: "general",
+              profileId: profile.id
+            }
+          ]
+        },
+        members: {
+          create: [
+            {
+              profileId: profile.id,
+              role: MemberRole.ADMIN
+            }
+          ]
+        }
+      }
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[SERVERS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
